0d4083778a54cd4804360e4372d088a08f2eca74
HIENLTH 2023 <hyhien@gmail.com>
2023-11-11 14:38:51 +0700

    Add file BT03 Lab06

---
+++ Buoi09_11112023/Lab06_BT03.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab06 - BT03</title>
+</head>
+<body>
+    <form method="post">
+        <h2>XEM THỰ MỤC HÌNH ẢNH</h2>
+        <div>
+            Chọn thư mục hình:
+            <select name="ten_thu_muc">
+                <option value="cake">cake</option>
+                <option value="flower">flower</option>
+                <option value="gift">gift</option>
+            </select>
+        </div>
+        <div>
+            Chọn kích cỡ:
+            <select name="kich_co">
+                <option value="50">50</option>
+                <option value="100">100</option>
+                <option value="150">150</option>
+                <option value="200">200</option>
+            </select>
+        </div>
+        <button>Xem hình ảnh</button>
+    </form>
+</body>
+</html>
